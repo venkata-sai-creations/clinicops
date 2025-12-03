dfda1fe3568255eb195545fe01dc2a464090eab3
venkata-sai-creations <tvsk1994@gmail.com>
2025-12-03 08:19:00 +0530

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,4 +1,7 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+variable "prefix" { type = string }
+variable "vpc_cidr" { type = string, default = "10.0.0.0/16" }
+variable "public_subnet_cidrs" { type = list(string), default = ["10.0.1.0/24","10.0.2.0/24"] }
+variable "private_subnet_cidrs" { type = list(string), default = ["10.0.3.0/24","10.0.4.0/24"] }
+variable "azs" { type = list(string), default = [] } # provider will fill if empty
+variable "enable_nat" { type = bool, default = false }
+variable "tags" { type = map(string), default = {} }
